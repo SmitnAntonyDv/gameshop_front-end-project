79385c0069b71581f160fd80df0a1a340a613209
SmitnAntonyDv <anton.smit@hotmail.com>
2020-06-12 18:09:47 +0200

    homepage initial functionality added

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,8 +3,18 @@ import React from "react";
 export default function Homepage() {
   return (
     <div>
-      <h1>I AM HOMEPAGE</h1>
+      <h1>The Hub</h1>
       <p>Hi i am component</p>
+      <div>
+        {"Categories: "}
+        <select>
+          <option>{"FPS"}</option>
+          <option>{"RTS"}</option>
+          <option>{"SPORTS"}</option>
+          <option>{"RPG"}</option>
+          <option>{"MMORPG"}</option>
+        </select>
+      </div>
     </div>
   );
 }
